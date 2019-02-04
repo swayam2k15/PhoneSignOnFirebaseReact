5c465175b1f56ff479a2817e25af3fab937cb02e
LL\swamohan0 <swamohan0@publicisgroupe.net>
2019-02-04 15:19:03 +0530

    Initial Firebase Phone Login Setup

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyARA8hD9ky28xtB-wg_tuIkWypCirjcd2I",
+    apiKey: "AIzaSyARA8hD9ky28xtB-bxakjsnxojsnxhoe",
     authDomain: "emp-project-d9a74.firebaseapp.com",
     databaseURL: "https://emp-project-d9a74.firebaseio.com",
     projectId: "emp-project-d9a74",
